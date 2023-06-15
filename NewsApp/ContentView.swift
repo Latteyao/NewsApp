0cd914277dccdc392a93127c47eb2d383ca1626c
Latteyao <qq8698192003@gmail.com>
2023-06-15 22:27:05 +0800

    Initial Commit

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by MacBook Pro on 2023/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by MacBook Pro on 2023/6/15.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
